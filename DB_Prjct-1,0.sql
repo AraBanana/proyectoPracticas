534ba5106bd1b18c7e15911b71abc04ed0d3c877
Arael Hidalgo <araelhidalgojuarez@gmail.com>
2023-10-19 12:21:31 -0600

    ASDASDASD

---
+++ DB_Prjct-1,0.sql
@@ -0,0 +1,10 @@
+--Script de bases de datos para control de documentos dentro de la coordinacion de prácticas profesionales:
+create database practProf;
+
+create table tipoSolicitud(
+	id_solicitud INT NOT NULL PRIMARY KEY,
+	
+
+
+
+);
