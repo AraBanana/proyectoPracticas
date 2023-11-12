85ab3735138cbd5c3d586ecbf94006129193008d
Arael Hidalgo <araelhidalgojuarez@gmail.com>
2023-11-12 10:33:20 -0600

    asasasasa

---
+++ Login/login.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const togglePassword = document.querySelector('.toggle-password');
+    const passwordInput = document.querySelector('.input[type="password"]');
+
+
+    togglePassword.addEventListener('click', function () {
+        const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+        passwordInput.setAttribute('type', type);
+    });
+});
